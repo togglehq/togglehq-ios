4d715b0a91664335f417811e850d707e7a22db59
Mike Fogg <me@mikefogg.co>
2016-09-23 11:01:15 -0400

    Attempting some more changes

---
+++ Example/ToggleExample/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ToggleExample
+//
+//  Created by Mike Fogg on 12/2/14.
+//  Copyright (c) 2014 Toggle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
